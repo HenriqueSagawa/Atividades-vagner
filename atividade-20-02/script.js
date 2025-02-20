618efc37968cd3edfb73fefd4f62a0db7b96cd6c
HenriqueSagawa <henriquetutomusagawa@gmail.com>
2025-02-20 00:23:57 -0300

    atividade 20-02 parte 1

---
+++ atividade-20-02/script.js
@@ -0,0 +1,22 @@
+const nav = document.getElementById('navbar');
+const btn = document.getElementsByClassName('open-button');
+
+const img = document.getElementById("imgBanner");
+const images = [
+    "https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1536104968055-4d61aa56f46a?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1668554245893-2430d0077217?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+]
+let index = 0;
+
+function changeImage() {
+    index = (index + 1) % images.length; 
+    img.src = images[index]; 
+}
+
+setInterval(changeImage, 7000) 
+
+function openMenu() {
+    nav.classList.toggle('open');
+}
